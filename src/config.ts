d9fa1b80bf70d435a198708edc4a5a04f7a9f959
Vitaliy Matveev <vmatveev@itgroup.lc>
2020-11-20 11:42:41 +0300

    add license to source files.

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,8 @@
+/** @license
+ * Copyright ©2020 Devexperts LLC. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 export const HEADER_AUTH_TOKEN_KEY = 'com.devexperts.auth.AuthToken'
