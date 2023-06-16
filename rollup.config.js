6d369b3929db0076efb281306cd4c4db0ff920fa
coolassassin <coolstreetassassin@gmail.com>
2023-06-16 18:59:56 +0400

    feat: add esm support

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import dts from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
+
+const name = 'lib/index'
+
+const bundle = config => ({
+  ...config,
+  input: 'src/index.ts',
+  external: id => !/^[./]/.test(id),
+})
+
+export default [
+  bundle({
+    plugins: [esbuild()],
+    output: [
+      {
+        file: `${name}.cjs`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `${name}.mjs`,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+  }),
+  bundle({
+    plugins: [dts()],
+    output: {
+      file: `${name}.d.ts`,
+      format: 'es',
+    },
+  }),
+]
